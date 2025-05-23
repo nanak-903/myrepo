f204e71f206ae34ccd98bfc25b29c3a49dafeb9b
Nanak Pabla <ndspabla@myseneca.ca>
2025-05-23 13:54:48 -0400

    Add lab2e.py

---
+++ ops445/lab2/lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Author: Nanak Dayal Singh Pabla
+# Author ID: 172318230
+# Date Created: 2025/05/23
+
+timer = 10
+while timer != 0:
+ print(timer)
+ timer = timer - 1
+print('blast off!')
