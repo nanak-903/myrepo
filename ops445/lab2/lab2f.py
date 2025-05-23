6bbc3e9211fe7b2f94f428654ff58d583b53d707
Nanak Pabla <ndspabla@myseneca.ca>
2025-05-23 13:55:11 -0400

    Add lab2f.py

---
+++ ops445/lab2/lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Nanak Dayal Singh Pabla
+# Author ID: 172318230
+# Date Created: 2025/05/23
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+ print(timer)
+ timer = timer - 1
+print('blast off!')
