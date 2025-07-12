995952b67023295e7fbdab4b9d292b490091d065
Nanak Pabla <ndspabla@myseneca.ca>
2025-07-11 20:59:21 -0400

    Add lab5a.py

---
+++ ops445/lab5/lab5a.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author ID: 172318230
+
+def read_file_string(file_name):
+    with open(file_name, 'r') as f:
+        return f.read()
+
+def read_file_list(file_name):
+    with open(file_name, 'r') as f:
+        return [line.strip('\n') for line in f]
+
+if __name__ == '__main__':
+    file_name = 'data.txt'
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
